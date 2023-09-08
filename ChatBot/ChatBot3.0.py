a668cb867c26878c2b34be30d0de8541d71eedc6
Bighi Alessandro <113016236+BighiAlessandro@users.noreply.github.com>
2023-09-08 13:19:01 +0200

    Add files via upload

---
+++ ChatBot/ChatBot3.0.py
@@ -0,0 +1,225 @@
+import sqlite3
+
+# Connessione al database o creazione se non esiste
+conn = sqlite3.connect('Dati_Offerte.db')
+
+# Creazione di un cursore
+cursor = conn.cursor()
+
+# Creazione di una tabella se non esiste
+cursor.execute('''CREATE TABLE IF NOT EXISTS offerte
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   Nome_Offerta TEXT,
+                   Descrizione TEXT,
+                   Stipendio INTEGER,
+                   Numero_Recruiter INTEGER)''')
+
+i = 1
+i1 = 0
+while i > 0:
+
+    print("Benvenuto su ChatBOT")
+    print("\n")
+    print("DatoreLavoro - Utente - Esci")
+    scelta = input("Come vuoi interagire? ")
+    scelta = scelta.upper()
+    if scelta == "DATORELAVORO":
+        print("\n")
+        password_inserita = input("Inserisci la password del DatoreLavoro: ")
+        
+        if password_inserita == "sasso":
+            print("Accesso consentito. Esegui azione per il caso DatoreLavoro")
+            print("\n")
+            print("Operazioni disponibili: Inserisci - Modifica - Elimina")
+            scelta1 = input("Come vuoi interagire? ")
+            scelta1 = scelta1.upper()
+            if scelta1 == "INSERISCI":
+                # Input dei dati dall'utente
+                print("\n")
+                Nome = input("Per che lavoro vuoi creare un'offerta?: ")
+                Descrizione = input("Inserisci una descrizione per il lavoro: ")
+                Stipendio = int(input("Inserisci lo stipendio: "))
+                Numero_Recruiter = int(input("Inserisci il numero a cui gli interessati ti potranno contattare: "))
+
+                # Inserimento dei dati nella tabella
+                cursor.execute("INSERT INTO offerte (Nome_Offerta, Descrizione, Stipendio, Numero_Recruiter) VALUES (?, ?, ?, ?)",
+                               (Nome, Descrizione, Stipendio, Numero_Recruiter))
+
+                # Salvataggio dei cambiamenti e chiusura della connessione
+                conn.commit()
+
+                print("\n")
+                print("Dati inseriti con successo nel database.")
+                print("\n")
+
+
+            elif scelta1 == "MODIFICA":
+                # Visualizza tutte le offerte disponibili per la modifica
+                cursor.execute("SELECT * FROM offerte")
+                offerte = cursor.fetchall()
+                print("Ecco le offerte disponibili per la modifica:")
+                for row in offerte:
+                    id, Nome, Descrizione, Stipendio, Numero_Recruiter = row
+                    print(f"ID: {id} | {Nome} | {Descrizione} | {Stipendio} EuroMensili | {Numero_Recruiter}")
+                
+                print("\n")
+
+                # Chiedi all'utente di selezionare l'ID dell'offerta da modificare
+                offerta_da_modificare = input("Inserisci l'ID dell'offerta da modificare: ")
+
+                # Verifica se l'ID inserito è valido
+                offerta_esistente = False
+                for row in offerte:
+                    id, _, _, _, _ = row
+                    if str(id) == offerta_da_modificare:
+                        offerta_esistente = True
+                        break
+
+                if not offerta_esistente:
+                    print("ID dell'offerta non valido. La modifica non è possibile.")
+                else:
+                    # Input dei nuovi dati dall'utente
+                    Nome = input("Inserisci il nuovo nome per l'offerta: ")
+                    Descrizione = input("Inserisci la nuova descrizione per l'offerta: ")
+                    Stipendio = int(input("Inserisci il nuovo stipendio: "))
+                    Numero_Recruiter = int(input("Inserisci il nuovo numero a cui gli interessati ti potranno contattare: "))
+
+                    # Esegui l'aggiornamento dei dati dell'offerta selezionata
+                    cursor.execute("UPDATE offerte SET Nome_Offerta=?, Descrizione=?, Stipendio=?, Numero_Recruiter=? WHERE id=?",
+                                   (Nome, Descrizione, Stipendio, Numero_Recruiter, offerta_da_modificare))
+
+                    # Salvataggio dei cambiamenti e chiusura della connessione
+                    conn.commit()
+
+                    print("\n")
+                    print("Offerta modificata con successo.")
+                    print("\n")
+
+            elif scelta1 == "ELIMINA":
+                # Visualizza tutte le offerte disponibili per l'eliminazione
+                cursor.execute("SELECT * FROM offerte")
+                offerte = cursor.fetchall()
+                print("Ecco le offerte disponibili per l'eliminazione:")
+                for row in offerte:
+                    id, Nome, Descrizione, Stipendio, Numero_Recruiter = row
+                    print(f"ID: {id} | {Nome} | {Descrizione} | {Stipendio} EuroMensili | {Numero_Recruiter}")
+
+                print("\n")
+                # Chiedi all'utente di selezionare l'ID dell'offerta da eliminare
+                offerta_da_eliminare = input("Inserisci l'ID dell'offerta da eliminare: ")
+                
+
+                # Verifica se l'ID inserito è valido
+                offerta_esistente = False
+                for row in offerte:
+                    id, _, _, _, _ = row
+                    if str(id) == offerta_da_eliminare:
+                        offerta_esistente = True
+                        break
+
+                if not offerta_esistente:
+                    print("ID dell'offerta non valido. L'eliminazione non è possibile.")
+                else:
+                    # Esegui l'eliminazione dell'offerta selezionata
+                    cursor.execute("DELETE FROM offerte WHERE id=?", (offerta_da_eliminare,))
+    
+                    cursor.execute("SELECT id FROM offerte")
+                    rows = cursor.fetchall()
+                    new_id = 1
+
+                    for row in rows:
+                        old_id = row[0]
+                        if old_id != new_id:
+                            cursor.execute("UPDATE offerte SET id=? WHERE id=?", (new_id, old_id))
+                            new_id += 1
+
+                    # Salvataggio dei cambiamenti e chiusura della connessione
+                    conn.commit()
+                    
+                    print("\n")
+                    print("Offerta eliminata con successo.")
+                    print("\n")
+
+            else:
+                print("Azione non valida.")
+        else:
+            print("Password errata. Accesso negato.")
+    
+    elif scelta == "UTENTE":
+        print("\n")
+        print("Opzioni disponibili: Visualizza - Cerca - Esci")
+        scelta1 = input("Come vuoi interagire? ")
+        print("\n")
+        scelta1 = scelta1.upper()
+        if scelta1 == "VISUALIZZA":
+            cursor.execute("SELECT id, Nome_Offerta, Stipendio FROM offerte")
+
+            # Recupero di tutti i dati e visualizzazione
+            for row in cursor.fetchall():
+                id, Nome, Stipendio = row
+                print(f"ID: {id} | {Nome} | {Stipendio} EuroMensili")
+            print("\n")
+
+            while i > 0:
+
+                scelta2 = input("VisualizzaID - Esci? ")
+                scelta2 = scelta2.upper()
+                if scelta2 == "VISUALIZZAID":
+                
+                
+                    a = input("Qual è l'id dell'offerta a cui sei interessato? ")
+
+                    print("\n")
+
+                    cursor.execute("SELECT * FROM offerte WHERE id = ?", (a,))
+                    offerte = cursor.fetchall()
+                    for row in offerte:
+                        id, Nome, Descrizione, Stipendio, Numero_Recruiter = row
+                        print(f"ID: {id} | {Nome} | {Descrizione} | {Stipendio} EuroMensili | {Numero_Recruiter}")
+                        print("\n")
+                
+                else:
+                    break
+
+
+        elif scelta1 == "CERCA":
+
+            print("\n")
+
+            parola = input("Inserisci parametro della tua ricerca: ")
+            l1 = parola.split()
+            x = len(l1)
+            
+            while i1<x:
+                cursor.execute("SELECT * FROM offerte WHERE Nome_Offerta LIKE ? OR Descrizione LIKE ? OR Stipendio LIKE ?", (f"%{l1[i1]}%", f"%{l1[i1]}%", f"%{l1[i1]}%"))
+                offerte = cursor.fetchall()
+                for row in offerte:
+                    id, Nome, Descrizione, Stipendio, Numero_Recruiter = row
+                    print(f"ID: {id} | {Nome} | {Descrizione} | {Stipendio} EuroMensili")
+
+                print("\n")
+                i1 = i1+1
+            i1 = 0
+            if offerte != {}:
+                scelta3 = input("Vuoi visualizzare il recapito telefonico? Si - No : ")
+                scelta3 = scelta3.upper()
+                if scelta3 == "SI":
+                    while i1<x:
+                        cursor.execute("SELECT * FROM offerte WHERE Nome_Offerta LIKE ? OR Descrizione LIKE ? OR Stipendio LIKE ?", (f"%{l1[i1]}%", f"%{l1[i1]}%", f"%{l1[i1]}%"))
+                        offerte = cursor.fetchall()
+                        cursor.execute("SELECT Numero_Recruiter FROM offerte WHERE Nome_Offerta LIKE ? OR Descrizione LIKE ? OR Stipendio LIKE ?", (f"%{l1[i1]}%", f"%{l1[i1]}%", f"%{l1[i1]}%"))
+                        recruiter = cursor.fetchall()
+                        for row in recruiter:
+                            Numero_Recruiter = row[0]
+                            print(f"Contatto telefonico del Recruiter: {Numero_Recruiter}")
+                        i1 = i1+1
+                    print("\n")
+        
+                else:
+                    break
+        
+    
+    elif scelta == "ESCI":
+        break
+
+conn.close()
